a3651d9214f18799dc3e64844d8202979f5e9481
beetmun <92041618+beetmun@users.noreply.github.com>
2023-04-19 17:32:36 +0800

    Add files via upload
    blog

---
+++ blog/client/src/components/logo/index.js
@@ -0,0 +1,9 @@
+import "./style.css"
+export function Logo(){
+    return(
+        <img
+            className="logo"
+            src="https://blog.hootsuite.com/wp-content/uploads/2020/02/Image-copyright.png"
+            alt="" />
+    )
+}
